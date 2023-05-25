2573b7fe55fe995e11ebb7b5d754a2dd449fa0f7
Ali Karaoglan <karaoglan.a.m@gmail.com>
2023-05-25 20:20:36 +0300

    last update

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -19,6 +19,23 @@ describe('Buttons', () =>{
             cy.contains('Clicked on button three!').should('be.visible');
         })
 
+        cy.get('button').each((item,index,list)=>{
+
+            expect(list).to.have.length(6);
+            expect(item).to.have.attr('onclick');
+        })
+        cy.get('button').each((item)=>{
+
+            if(item.text()=="Button 4"){
+
+                cy.log(item.text());
+                //item.click();
+                cy.wrap(item).click();
+                cy.contains('Clicked on button four!').should('be.visible');
+            }
+          
+        })
+
     })
 
 })
